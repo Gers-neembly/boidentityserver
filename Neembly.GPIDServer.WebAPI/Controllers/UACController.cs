faa481f7e62f40709c1d978e8c5e14240e69ec58
Jessa Delo-ag <jessadeloag@neembly.com>
2020-03-19 13:09:28 +0800

    Get Menu List API

---
+++ Neembly.GPIDServer.WebAPI/Controllers/UACController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Neembly.BOIDServer.SharedClasses;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Neembly.BOIDServer.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UACController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (StreamReader r = new StreamReader("uacmenulist.json"))
+            {
+                string json = r.ReadToEnd();
+                List<UACMenuInfo> menus = JsonConvert.DeserializeObject<List<UACMenuInfo>>(json);
+                return new JsonResult(menus);
+            }
+        }
+    }
+}
