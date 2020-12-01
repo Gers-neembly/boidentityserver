4c9e3a7c635cd24126299549660425c0dadf0c59
kevin <kevinmarkandrada@gmail.com>
2020-12-01 10:51:00 +0800

    dependency injection for BO Identity Server

---
+++ Neembly.GPIDServer.Persistence/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Neembly.BOIDServer.Persistence.Helpers;
+using Neembly.BOIDServer.Persistence.Interfaces;
+
+namespace Neembly.BOIDServer.Persistence
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// DI for persistence
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection Add(this IServiceCollection services) 
+        {
+            //data access
+            services.AddScoped<IDataAccess, DataAccess>();
+
+            return services;
+        }
+    }
+}
---
+++ Neembly.GPIDServer.SharedClasses/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Neembly.BOIDServer.SharedClasses
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// DI for shared classes - config
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection Add(this IServiceCollection services, IConfiguration configuration)
+        {
+            // authentication token info config
+            var authTokenConfig = new AuthTokenInfo();
+            configuration.Bind("AuthTokenInfo", authTokenConfig);
+            services.AddSingleton(authTokenConfig);
+
+            return services;
+        }
+    }
+}
